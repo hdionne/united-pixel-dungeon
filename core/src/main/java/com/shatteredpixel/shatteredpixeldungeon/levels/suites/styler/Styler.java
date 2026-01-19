e426fd5f171e020aac9b31de2e50619643ef6db8
Henry Dionne <henrydionne@proton.me>
2026-01-18 20:32:34 -0500

    Removed Styler Class. The Room class has its own paint method, for painting across a room, while the Painter class is for painting across entire levels. As such, the Styler class is not needed, and its functionality can simply be achieved by having multiple types of Suites.

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/suites/styler/Styler.java
+++
@@ -1,9 +0,0 @@
-package com.shatteredpixel.shatteredpixeldungeon.levels.suites.styler;
-
-import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
-
-// This is a way to style multiple rooms at once. These are similar to a Painter for a Level, except that these just apply to a single room
-// and painters can still occur over top them.
-public abstract class Styler extends Painter {
-
-}
